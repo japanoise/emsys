201946c28920f1b9aeb5dbbdfccbbe32a06d2c34
japanoise <japanoise@seekrit.club>
2021-04-21 19:31:36 +0200

    Add screen handling code
    Up to about halfway through stage 3 of the tutorial

--- main.c
+++ main.c
@@ -2,64 +2,162 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<sys/ioctl.h>
 #include<termios.h>
 #include<unistd.h>
+
+/*** util ***/
 
 #define ESC "\033"
 #define CSI ESC"["
 #define CRLF "\r\n"
 #define ISCTRL(c) ((0 < c && c < 0x20) || c == 0x7f)
-
-struct termios orig_termios;
+#define CTRL_KEY(c) ((c)&0x1f)
+#define PANIC CTRL_KEY('p')
 
 void die(const char *s) {
-        perror(s);
-        exit(1);
+	write(STDOUT_FILENO, CSI"2J", 4);
+	write(STDOUT_FILENO, CSI"H", 3);
+	perror(s);
+	write(STDOUT_FILENO, CRLF, 2);
+	write(STDOUT_FILENO, "sleeping 5s", 11);
+	sleep(5);
+	exit(1);
 }
 
+/*** data ***/
+
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+/*** terminal ***/
+
 void disableRawMode() {
-        if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1)
-                die("disableRawMode tcsetattr");
-        if(write(STDOUT_FILENO, CSI"?1049l", 8)==-1)
-                die("disableRawMode write");
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios)==-1)
+		die("disableRawMode tcsetattr");
+	if(write(STDOUT_FILENO, CSI"?1049l", 8)==-1)
+		die("disableRawMode write");
 }
 
 void enableRawMode() {
-        /* Saves the screen and switches to an alt screen */
-        if(write(STDOUT_FILENO, CSI"?1049h", 8)==-1)
-                die("disableRawMode write");
-        /* 
-         * I looked into it. It's possible, but not easy, to do it
-         * without termios. Basically you'd have to hand-hack and send
-         * off your own bits. Check out busybox vi and that rabbithole
-         * for an implementation. 
-         */
-        if(tcgetattr(STDIN_FILENO, &orig_termios)==-1)
-                die("tcgetattr");
-        atexit(disableRawMode);
+	/* Saves the screen and switches to an alt screen */
+	if(write(STDOUT_FILENO, CSI"?1049h", 8)==-1)
+		die("enableRawMode write");
+	/* 
+	 * I looked into it. It's possible, but not easy, to do it
+	 * without termios. Basically you'd have to hand-hack and send
+	 * off your own bits. Check out busybox vi and that rabbithole
+	 * for an implementation. 
+	 */
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios)==-1)
+		die("tcgetattr");
+	atexit(disableRawMode);
 
-        struct termios raw = orig_termios;
-        raw.c_iflag &= ~(BRKINT|ICRNL|INPCK|ISTRIP|IXON);
-        raw.c_oflag &= ~(OPOST);
-        raw.c_cflag |= (CS8);
-        raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG);
-        if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1)
-                die("enableRawMode tcsetattr");
+	struct termios raw = E.orig_termios;
+	raw.c_iflag &= ~(BRKINT|ICRNL|INPCK|ISTRIP|IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1)
+		die("enableRawMode tcsetattr");
+}
+
+/* Raw reading a keypress */
+int editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	if (c == PANIC) {
+		errno = EINTR;
+		die("Panic key");
+	}
+	return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+	buf[i] = '\0';
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+/*** output ***/
+
+void editorDrawRows() {
+	int y;
+	for (y=0; y<E.screenrows; y++) {
+		write(STDOUT_FILENO, CSI"34m~"CSI"0m", 10);
+		if (y<E.screenrows - 1) {
+			write(STDOUT_FILENO, CRLF, 2);
+		}
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, CSI"2J", 4);
+	write(STDOUT_FILENO, CSI"H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, CSI"H", 3);
+}
+
+/*** input ***/
+
+/* Where the magic happens */
+void editorProcessKeypress() {
+	int c = editorReadKey();
+
+	switch (c) {
+		case CTRL('q'):
+			exit(0);
+			break;
+	}
+}
+
+/*** init ***/
+
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main(int argc, char *argv[]) {
-        enableRawMode();
-        char c;
-        while (1) {
-                if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-                        die("read");
-                if (ISCTRL(c)) {
-                        printf("%d"CRLF, c);
-                } else {
-                        printf("%d ('%c')"CRLF, c, c);
-                }
-                if (c == 'q') break;
-        }
-        return 0;
+	enableRawMode();
+	initEditor();
+
+	char c;
+	while (1) {
+		editorRefreshScreen();
+		editorProcessKeypress();
+	}
+	return 0;
 }
 
