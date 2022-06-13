429c812765e8048ae9a0832c62ee8ad45587cfe9
japanoise <japanoise@seekrit.club>
2022-06-12 23:16:55 -0500

    Use subprocess.h for pipe
    This works much better! Shout out to the subprocess.h maintainers.

--- pipe.c
+++ pipe.c
@@ -9,138 +9,51 @@
 #include "emsys.h"
 #include "region.h"
 #include "pipe.h"
+#include "subprocess.h"
 
 static uint8_t *cmd;
 static char *buf;
 
 static uint8_t *transformerPipeCmd(uint8_t *input) {
-	struct timeval tv;
-	int pin[2], pout[2], perr[2], nr = 1, nerr = 1, nw, written;
-	int iw = 0, closed = 0, exstatus;
-	char *ebuf = NULL;
-	/* Filepos auxp; */
-	fd_set fdI, fdO;
-	pid_t pid = -1;
-
-	/* XXX: Returning input is potentially unsafe (double-free?) */
-	if(cmd[0] == '\0')
-		return input;
-	if(pipe(pin) == -1)
-		return input;
-	if(pipe(pout) == -1) {
-		close(pin[0]);
-		close(pin[1]);
-		return input;
+	int bsiz = BUFSIZ+1;
+	/* Using sh -c lets us use pipes and stuff and takes care of quoting. */
+	const char *command_line[4] = {"/bin/sh", "-c", (char*)cmd, NULL};
+	struct subprocess_s subprocess;
+	int result = subprocess_create(
+		command_line,
+		subprocess_option_inherit_environment,
+		&subprocess);
+	if (result) {
+		die("subprocess");
 	}
-	if(pipe(perr) == -1) {
-		close(pin[0]);
-		close(pin[1]);
-		close(pout[0]);
-		close(pout[1]);
-		return input;
+	FILE* p_stdin = subprocess_stdin(&subprocess);
+	FILE* p_stdout = subprocess_stdout(&subprocess);
+
+	/* Write region to subprocess */
+	for (int i = 0; input[i]; i++) {
+		fputc(input[i], p_stdin);
 	}
 
-	/* i_sortpos(&fsel, &fcur); */
+	/* Join process */
+	int sub_ret;
+	subprocess_join(&subprocess, &sub_ret);
 
-	/* Things I will undo or free at the end of this function */
-	/* s = i_gettext(fsel, fcur); */
-
-	if((pid = fork()) == 0) {
-		dup2(pin[0], 0);
-		dup2(pout[1], 1);
-		dup2(perr[1], 2);
-		close(pin[0]);
-		close(pin[1]);
-		close(pout[0]);
-		close(pout[1]);
-		close(perr[0]);
-		close(perr[1]);
-		/* I actually like it with sh so I can input pipes et al. */
-		execl("/bin/sh", "sh", "-c", cmd, NULL);
-		fprintf(stderr, "sandy: execl sh -c %s", cmd);
-		perror(" failed");
-		exit(EXIT_SUCCESS);
-	}
-
-	if(pid > 0) {
-		close(pin[0]);
-		close(pout[1]);
-		close(perr[1]);
-
-		fcntl(pin[1], F_SETFL, O_NONBLOCK);
-		fcntl(pout[0], F_SETFL, O_NONBLOCK);
-		fcntl(perr[0], F_SETFL, O_NONBLOCK);
-
-		ebuf = calloc(1, BUFSIZ + 1);
-
-		FD_ZERO(&fdO);
-		FD_SET(pin[1], &fdO);
-		FD_ZERO(&fdI);
-		FD_SET(pout[0], &fdI);
-		FD_SET(perr[0], &fdI);
-		tv.tv_sec = 5;
-		tv.tv_usec = 0;
-		nw = strlen((char*)input);
-		while(select(FD_SETSIZE, &fdI, &fdO, NULL, &tv) > 0 &&
-		     (nw > 0 || nr > 0)) {
-			fflush(NULL);
-			if(FD_ISSET(pout[0], &fdI) && nr > 0) {
-				nr = read(pout[0], buf, BUFSIZ);
-				if(nr >= 0)
-					buf[nr] = '\0';
-				else
-					break; /* ...not seen it yet */
-
-				if (nr) {
-					editorSetStatusMessage("nr %i buf %s", nr, buf);
-				}
-			} else if(nr > 0) {
-				FD_SET(pout[0], &fdI);
-			} else {
-				FD_CLR(pout[0], &fdI);
-			}
-			if(FD_ISSET(perr[0], &fdI) && nerr > 0) {
-				/* Blatant TODO: take last line of stderr and copy as tmptitle */
-				ebuf[0] = '\0';
-				nerr = read(perr[0], ebuf, BUFSIZ);
-				if(nerr == -1)
-					editorSetStatusMessage("WARNING! command reported an error!!!");
-				if(nerr < 0)
-					break;
-			} else if(nerr > 0) {
-				FD_SET(perr[0], &fdI);
-			} else {
-				FD_CLR(perr[0], &fdI);
-			}
-			if(FD_ISSET(pin[1], &fdO) && nw > 0) {
-				written = write(pin[1], &(input[iw]),
-				    (nw < BUFSIZ ? nw : BUFSIZ));
-				if(written < 0)
-					break; /* broken pipe? */
-				iw += (nw < BUFSIZ ? nw : BUFSIZ);
-				nw -= written;
-			} else if(nw > 0) {
-				FD_SET(pin[1], &fdO);
-			} else {
-				if(!closed++)
-					close(pin[1]);
-				FD_ZERO(&fdO);
-			}
+	/* Read stdout of process into buffer */
+	int c = fgetc(p_stdout);
+	int i = 0;
+	while (c != EOF) {
+		buf[i++] = c;
+		buf[i] = 0;
+		if (i >= bsiz-10) {
+			bsiz <<= 1;
+			buf = realloc(buf, bsiz);
 		}
-		free(ebuf);
-		if(!closed)
-			close(pin[1]);
-		waitpid(pid, &exstatus, 0); /* We don't want to close the pipe too soon */
-		close(pout[0]);
-		close(perr[0]);
-	} else {
-		close(pin[0]);
-		close(pin[1]);
-		close(pout[0]);
-		close(pout[1]);
-		close(perr[0]);
-		close(perr[1]);
+		c = fgetc(p_stdout);
 	}
+	editorSetStatusMessage("Read %d bytes", i);
+
+	/* Cleanup & return */
+	subprocess_destroy(&subprocess);
 	return (uint8_t*) buf;
 }
 
@@ -150,6 +63,7 @@ void editorPipe(struct editorConfig *ed, struct editorBuffer *bf) {
 	cmd = editorPrompt(bf, (uint8_t*)"Pipe command: %s", NULL);
 	if (cmd == NULL) {
 		editorSetStatusMessage("Canceled pipe command.");
+		free(buf);
 		return;
 	}
 
