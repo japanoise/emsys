943ab8aaff92b038635ceb273eb75a45dd3ae894
japanoise <japanoise@seekrit.club>
2022-06-28 11:25:55 -0500

    Use ranges to define word boundaries
    Most of ASCII outside of the alphanumerics and some punctuation is a
    boundary of some kind; use ranges to express this. This is a big
    improvement; I think the usual expectation is it's only alphanums and
    a small handful of punctuation that counts as word characters. This
    shouldn't give us any performance degradation either, as the number of
    expressions has actually decreased (11->9).

--- bound.c
+++ bound.c
@@ -3,9 +3,12 @@
 #include "emsys.h"
 
 int isWordBoundary(uint8_t c) {
-	return (c == ' ' || c == CTRL('i') || c == '_' || c == 10 || c == 0 ||
-		c == '(' || c == ')' || c == '[' || c == ']' ||
-		c == '{' || c == '}');
+	return !(c > '~') && 	/* Anything outside ASCII is not a boundary */
+		!('a' <= c && c <= 'z') && /* Lower case ASCII not boundaries */
+		!('A' <= c && c <= 'Z') && /* Same with caps */
+		!('0' <= c && c <= '9') && /* And numbers */
+		((c < '$') || 	/* ctrl chars & some punctuation */
+		 (c > '%'));	/* Rest of ascii outside $% & other ranges */
 }
 
 int isParaBoundary(erow *row) {
