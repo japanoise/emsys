823dca460c6c02e722f1b71b351f27cb538df579
japanoise <japanoise@users.noreply.github.com>
2024-07-18 08:03:05 -0500

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,6 +1,9 @@
 #ifndef _EMSYS_CONFIG_H
 #define _EMSYS_CONFIG_H
 
+/* Make a copy of this file, call it config.h, and change it however you like */
+/* By default, it will be copied as-is by the makefile. */
+
 /* Universal Argument C-u */
 /* #define EMSYS_CU_UARG */
 
